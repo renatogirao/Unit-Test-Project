b18764847bec22dc8a519e424004dccf893764a1
Renato Girão <renato.sgirao@gmail.com>
2021-11-10 18:32:19 -0300

    created string extension

---
+++ StringUtils.swift
@@ -0,0 +1,26 @@
+//
+//  StringUtils.swift
+//  CursoTests
+//
+//  Created by Renato Savoia Girão on 10/11/21.
+//
+
+import Foundation
+
+extension String {
+    
+    func removeWhiteSpace() -> String {
+        
+        return self.replace(string: "", replacement: "")
+    }
+    
+    func replace(string: String, replacement: String) -> String {
+        return self.replacingOccurrences(of: string, with: replacement, options: .literal, range: nil)
+    }
+    
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z](2,)"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+}
